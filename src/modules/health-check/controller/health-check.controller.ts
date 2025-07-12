523a405df75262175cc8e23ca6ea7fc0de7c108d
EleazarGamezD <eleazar.gamezd@gmail.com>
2025-07-12 16:15:17 -0500

    refactor: clean up health check controller and remove redundant tags

--- src/modules/health-check/controller/health-check.controller.ts
+++ src/modules/health-check/controller/health-check.controller.ts
@@ -6,16 +6,15 @@ import {HealthCheckServerService} from '../services/health-check.service';
 @ApiTags('Server Health')
 @Controller('health-check')
 export class HealthCheckController {
-  constructor(private readonly healthCheckService: HealthCheckServerService) {}
+  constructor(private readonly healthCheckService: HealthCheckServerService) { }
 
   @Get()
   @HealthCheck()
-  @ApiTags('Liveness')
   @ApiTags('Server Health')
-/*   @ApiBearerAuth('token')
-  @Auth(RolesEnum.ADMIN, RolesEnum.SUPER_ADMIN)
-  @UseGuards(AuthGuard('jwt'))
-  @PermissionsDecorator(PermissionsEnum.CREATE_USER) */
+  /*   @ApiBearerAuth('token')
+    @Auth(RolesEnum.ADMIN, RolesEnum.SUPER_ADMIN)
+    @UseGuards(AuthGuard('jwt'))
+    @PermissionsDecorator(PermissionsEnum.CREATE_USER) */
   getLiveness() {
     return this.healthCheckService.servicesHealth();
   }
